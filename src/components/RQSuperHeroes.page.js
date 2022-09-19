17a48e2dca7f862e277ea92942c64e6df849af88
AlAminKh03 <alaminkhan6203@gmail.com>
2022-09-19 23:49:11 +0600

    axios and query mixed up done

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,25 @@
+import axios from "axios"
+import { useQuery } from "react-query"
+
+const getSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes1")
+}
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
+
+  const { isLoading, data, isError, error } = useQuery(
+    "superHeros",
+    getSuperHeroes
+  )
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  }
+  if (isError) {
+    return <h2>{error.message}</h2>
+  }
+  return <>
+    <h2>React Query Super Heroes Page</h2>
+    {data?.data.map(hero => {
+      return <p>{hero.name}</p>
+    })}
+  </>
 }
