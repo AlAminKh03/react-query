7ce1606f4a6e20628335ce93a9aa6b4efde52e4d
AlAminKh03 <alaminkhan6203@gmail.com>
2022-09-27 20:33:27 +0600

    infinite quieries

--- src/components/InfintieQueries.page.js
+++ src/components/InfintieQueries.page.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import axios from 'axios';
+import React, { Fragment } from 'react';
+import { useInfiniteQuery } from 'react-query';
 
+const fetchInfiniteQueries = ({ pageParam = 1 }) => {
+    return axios.get(`http://localhost:4000/colors?_limit=2&_page=${pageParam}`)
+}
 const InfintieQueries = () => {
+    const { isLoading, data, isError, error, hasNextPage, fetchNextPage } = useInfiniteQuery(
+        'colors', fetchInfiniteQueries, {
+        getNextPageParam: (_lastPage, pages) => {
+            if (pages.length < 4) {
+                return pages.length + 1
+            }
+            else {
+                return undefined
+            }
+        }
+    }
+    )
+    console.log(data?.pages);
     return (
         <div>
-
+            {data?.pages.map((group, i) => {
+                return <Fragment key={i}>
+                    {group?.data.map(color => {
+                        return <div key={color.id}>
+                            <h1>{color.id} - {color.label}</h1>
+                        </div>
+                    })}
+                </Fragment>
+            })}
+            <button disabled={!hasNextPage} onClick={fetchNextPage}>load more...</button>
         </div>
     );
 };
